cb14db2d403f32b75600b1425b45f7834c1c6954
Hana <hanalways@gmail.com>
2019-03-04 07:45:30 -0800

    finish robot name

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,28 @@
+# Robot Name
 
+class Robot
+  attr_reader :name
+
+  def initialize
+    @letters_array = ("A".."Z").to_a
+    @numbers_array = ("0".."9").to_a
+
+    @name = (@letters_array.sample(2) + @numbers_array.sample(3)).join("")
+
+    return @name
+  end
+
+  def reset
+    rand_name = (@letters_array.sample(2) + @numbers_array.sample(3)).join("")
+    until rand_name != @name
+      rand_name = (@letters_array.sample(2) + @numbers_array.sample(3)).join("")
+    end
+
+    @name = rand_name
+    return @name
+  end
+end
+
+robo = Robot.new.name
+
+puts robo
